4e674265557e110f7737e8d106fecb4307b78bbb
zhangpengsheng <760726674@qq.com>
2020-06-09 19:22:37 +0800

    update

---
+++ element-ui-test/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <!-- <HelloWorld msg="hello world vue" /> -->
+    <!-- basic -->
+    <Basic />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Basic from "@/components/Basic.vue";
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld,
+    Basic
+  }
+};
+</script>
